28885425339ca1ef79c10de98d726f89c43af512
Carlosfromsubway <tlee285@my.bcit.ca>
2023-02-19 14:47:35 -0800

    inittial

---
+++ components/button.js
@@ -0,0 +1,23 @@
+import styled from "styled-components"
+
+export default function NasaButton() {
+    return (
+        <div> 
+            <Button/>
+        </div>
+    )
+}
+
+const Button = styled.button `
+
+width: 200px;
+height: 40px;
+background-color: transparent;
+border: solid 1px;
+color: transparent;
+border-color: #3A9BDC;
+font-color: white;
+font-size: 20px
+font-weight: bold;
+
+`
